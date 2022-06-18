cda5556c459121534ae3429b18fe1529476b9fbf
UnknownGamer <68790263+UnknownGamer696@users.noreply.github.com>
2022-06-17 18:34:37 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,12 @@
+-- local l = loadstring(game:HttpGet("https://raw.githubusercontent.com/UnknownGamer696/Space-Hub/main/UILibrary.lua"))()
+-- local win = l:New({
+-- 	Name = "Space Hub",
+-- 	FolderToSave = "SpaceHub"
+-- })
+-- local home = win:Tab("Home")
+-- local key = home:Section("Key")
 
+local gameId = game.GameId
+local placeId = game.PlaceId
+
+loadstring(game.HttpGet("https://raw.githubusercontent.com/UnknownGamer696/Space-Hub/main/games/" .. gameId or placeId or "Universal"))()
